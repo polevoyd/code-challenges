bd14682f67acb33c5f1853b8790045fac9f952af
Dmitry <manxxeta@gmail.com>
2018-09-12 18:42:47 -0700

    13

--- day3-challange/challenges-03.test.js
+++ day3-challange/challenges-03.test.js
@@ -112,17 +112,19 @@ const nestedArray = [ [ [1, 2, 3], [4, 5, 6] ], [ [7, 8, 9], [10, 11, 12] ], [ [
 const findFourteen = (array) => {
   // Solution code here...
   
-  for (let i of array)
-  {
-    for (let k of i)
-    {
-      for (let l of k)
-      {
-        if (l === 14)
-          return l;
-      }
-    }
-  }
+//   for (let i of array)
+//   {
+//     for (let k of i)
+//     {
+//       for (let l of k)
+//       {
+//         if (l === 14)
+//           return l;
+//       }
+//     }
+//   }
+
+  return array[2][0][1];
 };
 
 // ------------------------------------------------------------------------------------------------
