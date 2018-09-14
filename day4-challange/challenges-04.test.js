e90d80278407250c738bb4db2c6b3f24372d6b8f
Dmitry <manxxeta@gmail.com>
2018-09-14 15:32:17 -0700

    4

--- day4-challange/challenges-04.test.js
+++ day4-challange/challenges-04.test.js
@@ -71,7 +71,8 @@ const citiesAtoJ = (cities) => {
 
 const matchMonth = (input) => {
   // Solution code here...
-//   return input.contains('oct') || input.contains('Oct');
+  let nov = ['October', 'Oct', 'october', 'oct'];
+  return nov.includes(input) ? true : false;
 };
 
 // ------------------------------------------------------------------------------------------------
