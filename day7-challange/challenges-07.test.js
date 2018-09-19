942aef4db191ca54686b688c939df8dc1a539049
Dmitry <manxxeta@gmail.com>
2018-09-18 19:12:35 -0700

    3 done

--- day7-challange/challenges-07.test.js
+++ day7-challange/challenges-07.test.js
@@ -38,12 +38,17 @@ const forEachTwoToThe = (input) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1 and your forEachTwoToThe function from challenge 2, but uses map instead of a for loop or forEach.
+Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1 and your forEachTwoToThe 
+function from challenge 2, but uses map instead of a for loop or forEach.
 ------------------------------------------------------------------------------------------------*/
 
 const mapTwoToThe = (input) => {
   // Solution code here...
-}
+  return input.map(function(element)
+  {
+    return Math.pow(2, element);
+  });
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
