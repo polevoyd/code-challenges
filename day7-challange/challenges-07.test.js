6fbc9719cb9e772c31510e99e35dc79107629dbf
Dmitry <manxxeta@gmail.com>
2018-09-18 19:36:01 -0700

    5 done

--- day7-challange/challenges-07.test.js
+++ day7-challange/challenges-07.test.js
@@ -3,7 +3,7 @@
 /*------------------------------------------------------------------------------------------------
 CHALLENGE 1
 
-Write a function named forLoopTwoToThe that, given an array of integers as input, iterates over the array and returns a new array. 
+Write a function named forLoopTwoToThe that, given an array of integers as input, iterates over the array and returns a new array.
 The returned array should containing the result of raising 2 to the power of the original input element.
 
 You may choose to complete this challenge using a for loop, for...in syntax, or for...of syntax.
@@ -38,7 +38,7 @@ const forEachTwoToThe = (input) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1 and your forEachTwoToThe 
+Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1 and your forEachTwoToThe
 function from challenge 2, but uses map instead of a for loop or forEach.
 ------------------------------------------------------------------------------------------------*/
 
@@ -53,7 +53,7 @@ const mapTwoToThe = (input) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
 
-Write a function named charCode that, given an array of letters as an input, uses map to return a new array where each element 
+Write a function named charCode that, given an array of letters as an input, uses map to return a new array where each element
 is the result of the `charCodeAt` method on the original array element.
 
 Read the MDN documentation on String.charCodeAt() if necessary.
@@ -73,7 +73,8 @@ const charCode = (input) => {
 /*------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
-Write a function that, given an array of numbers as input, uses map to return a new array where each element is either the string "even" or the string "odd", based on each value.
+Write a function that, given an array of numbers as input, uses map to return a new array where each
+element is either the string "even" or the string "odd", based on each value.
 
 If any element in the array is not a number, the resulting array should have the string "N/A" in its place.
 
@@ -81,7 +82,12 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 ------------------------------------------------------------------------------------------------*/
 
 const evenOdd = (input) => {
-  // Solution code here...
+
+  input.forEach(function(element, index, array)
+  {
+    array[index] = (Number.isInteger(element)) ? (element % 2 === 0) ? 'even' : 'odd' : 'N/A';
+  });
+  return input;
 };
 
 /*------------------------------------------------------------------------------------------------
